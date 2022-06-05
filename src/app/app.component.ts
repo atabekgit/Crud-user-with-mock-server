c37686dd81bd649bdb9f334075dc5ade71be3474
atabek <atabekgithub23@gmail.com>
2022-06-05 23:26:47 +0600

    server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Employee} from "./models/employee.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'crud-user';
+export class AppComponent implements AfterViewInit ,OnInit{
+ @ViewChild('tempButton') buttontemp:any;
+ employeeForm:FormGroup
+  employees:Employee[]
+  employeeToDisplay:Employee[]
+ educationOptions =[
+   '10th pass',
+   'diploma',
+   'graduate',
+   'post graduate',
+   'PhD',
+ ];
+ constructor(private fb:FormBuilder) {
+   this.employeeForm = fb.group({})
+   this.employees = []
+   this.employeeToDisplay = this.employees
+ }
+
+  ngAfterViewInit(): void {
+    this.buttontemp.nativeElement.click();
+  }
+
+  ngOnInit(): void {
+   this.employeeForm = this.fb.group({
+     firstName:this.fb.control(''),
+     lastName:this.fb.control(''),
+     birthday:this.fb.control(''),
+     gender:this.fb.control(''),
+     education:this.fb.control('default'),
+     company:this.fb.control(''),
+     jobExperience:this.fb.control(''),
+     salary:this.fb.control(''),
+
+   })
+  }
 }
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Employee} from "../models/employee.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getEmployees() {
+    return this.http.get<Employee[]>(`${environment.baseUrl}`);
+  }
+
+  postEmployees(employee: Employee) {
+    return this.http.post<Employee[]>(`${environment.baseUrl}`, employee);
+  }
+
+  deleteEmployees(id: string) {
+    return this.http.delete(`${environment.baseUrl} +'/'` + id);
+  }
+
+}
